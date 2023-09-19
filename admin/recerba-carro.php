5e217cb85d9d0023b7fc68b8f94cad71a98e002f
unknown <APRENDIZ@BOGCPCDMF11912.FormacionSena.RED>
2023-09-19 11:36:46 -0500

    ventana modal autos ocupados

--- admin/recerba-carro.php
+++ admin/recerba-carro.php
@@ -279,10 +279,32 @@ $('#btn_registrar_ticket<?php echo $id_map;?>').click(function () {
         <div class="col">
         <center>
             <h2><?php echo $nro_espacio;?></h2>
-            <button class="btn btn-danger">
-            <img src="images/auto1.png" width="60px" alt="">           
+            <button type="button" class="btn btn-danger" style="width: 100%;height: 114px" data-toggle="modal" data-target="#modal2_<?php echo $id_map;?>">
+            <img src="images/auto1.png" width="60px" alt="">
             </button>
             <p class="text-white"><?php echo $estado_espacio;?></p>
+            <!-- Button trigger modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="modal2_<?php echo $id_map;?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
         </center>
     </div>
 <?php
